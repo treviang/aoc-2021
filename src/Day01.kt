1e474a322b9e2c327008d9ac3069f339ee2ee822
Angelo Trevisiol <angelo.trevisiol@gmail.com>
2021-12-01 22:27:58 +0100

    part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,7 @@
 fun main() {
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return countIncreases(input);
     }
 
     fun part2(input: List<String>): Int {
@@ -9,9 +10,20 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    //println(countIncreases(testInput))
 
     val input = readInput("Day01")
     println(part1(input))
-    println(part2(input))
+    //println(part2(input))
+}
+
+fun countIncreases(input: List<String>): Int {
+    var count = 0
+
+    for ((index, value) in input.withIndex()) {
+        if(index > 0 && value > input[index-1]) {
+            count ++
+        }
+    }
+    return count;
 }
