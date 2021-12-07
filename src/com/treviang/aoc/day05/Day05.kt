a138375d998fe069ae5a589d0e04d3f9d68423aa
Angelo Trevisiol <angelo.trevisiol@gmail.com>
2021-12-07 01:14:10 +0100

    day 5 part 1

---
+++ src/com/treviang/aoc/day05/Day05.kt
@@ -0,0 +1,47 @@
+package com.treviang.aoc.day05
+
+import com.treviang.aoc.constants.EMPTY_STRING
+import readInput
+
+fun main() {
+
+    //fun part1(input: List<String>): Int {
+    //var power = com.treviang.aoc.day03.findPowerConsumption(input)
+    //return power.gammaRate.toInt() * power.epsilonRate.toInt()
+    //}
+
+    //fun part2(input: List<String>): Int {
+    //var lsr = com.treviang.aoc.day03.findLifeSupportRating(input)
+    //return lsr.oxygenRating * lsr.cO2Rating
+    //}
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("com/treviang/aoc/day05/Day05_test")
+    println(crossLine(testInput))
+
+    //val input = readInput("com/treviang/aoc/day05/Day05")
+    //println(part1(input))
+    //println(part2(input))
+}
+
+data class Point(var x: Int, var y: Int)
+data class Coordinate(var p1: Point, var p2: Point)
+
+fun crossLine(input: List<String>) {
+    var coordinates: MutableList<Coordinate> = mutableListOf()
+    input.forEach() {
+        var points = it.split("->").map { it ->
+            Point(it.split(",")[0].trim().toInt(), it.split(",")[1].trim().toInt())
+        }
+        coordinates.add(Coordinate(points[0], points[1]))
+    }
+    var grid = Array(10) { Array(10) { 0 } }
+
+    for (i in 0 until coordinates.size) {
+        if (coordinates[i].p1.x == coordinates[i].p2.x) {
+
+        } else if (coordinates[i].p1.y == coordinates[i].p2.y) {
+
+        }
+    }
+}
